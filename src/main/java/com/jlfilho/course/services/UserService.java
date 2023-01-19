bfb041b4b182f44e00052085db9a23d5695f1f8e
João da Mata <jlfilho@uea.edu.br>
2023-01-19 01:08:45 -0400

    Service layer, component registration

---
+++ src/main/java/com/jlfilho/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.jlfilho.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jlfilho.course.domain.User;
+import com.jlfilho.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id); 
+		return user.get();
+	}
+
+}
