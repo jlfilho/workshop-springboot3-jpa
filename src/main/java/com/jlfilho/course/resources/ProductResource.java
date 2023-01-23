a5b6200391d11f9b1a84757762e041e301528853
João da Mata <jlfilho@uea.edu.br>
2023-01-23 00:47:14 -0400

    Update EndPoint products

---
+++ src/main/java/com/jlfilho/course/resources/ProductResource.java
@@ -0,0 +1,36 @@
+package com.jlfilho.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jlfilho.course.domain.Product;
+import com.jlfilho.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> products = productService.findAll();
+		
+		return ResponseEntity.ok().body(products);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Long id) {
+		Product product = productService.findById(id);
+		
+		return ResponseEntity.ok().body(product);
+	}
+
+}
